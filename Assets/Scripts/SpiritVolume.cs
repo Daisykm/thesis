52e732b199ff0c76679ec2b96308bc81532e5fb0
Daisykm <56872925+Daisykm@users.noreply.github.com>
2021-04-11 21:08:40 +0200

    week 10

---
+++ Assets/Scripts/SpiritVolume.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiritVolume : MonoBehaviour
+{
+    public GameObject SpirtVolume;
+    private bool spiritVolumeActive;
+    public GameObject thirdPersonCamera;
+    public GameObject spiritVisionCamera;
+    //public GameObject playerMesh;
+  
+    void Start()
+    {
+        SpirtVolume.SetActive(false);
+        spiritVolumeActive = false;
+        
+        
+    }
+
+  
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && spiritVolumeActive == false)
+        {
+            SpirtVolume.SetActive(true);
+            spiritVolumeActive = true;
+            thirdPersonCamera.SetActive(false);
+            spiritVisionCamera.SetActive(true);
+          //  playerMesh.SetActive(false);
+        }
+        else if (Input.GetKeyDown(KeyCode.E) && spiritVolumeActive == true)
+        {
+            SpirtVolume.SetActive(false);
+            spiritVolumeActive = false;
+            thirdPersonCamera.SetActive(true);
+            spiritVisionCamera.SetActive(false);
+           // playerMesh.SetActive(true);
+        }
+    }
+}
