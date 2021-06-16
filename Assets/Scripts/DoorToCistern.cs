503810eb0040b54dfe9cf2599e889b7d2ecea8ad
Daisykm <56872925+Daisykm@users.noreply.github.com>
2021-06-16 23:59:14 +0200

    final week 3

--- Assets/Scripts/DoorToCistern.cs
+++ Assets/Scripts/DoorToCistern.cs
@@ -27,7 +27,7 @@ public class DoorToCistern : MonoBehaviour
     {
         if (colliding && Input.GetKeyDown(KeyCode.Q))
         {
-            SceneManager.LoadScene("21_Cisterne Update");
+            SceneManager.LoadScene("11_Cisterne");
         }
         
     }
